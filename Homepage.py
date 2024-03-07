0a2604e5f37d9139da5b9105508abf722255ca57
rikschipper <148549624+rikschipper@users.noreply.github.com>
2024-03-07 22:29:39 +0100

    Update Homepage.py

--- Homepage.py
+++ Homepage.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from Pages import Age_groups.py, Interests_per_continent.py, Conclusie,py
+from case-2/Pages import Age_groups, Interests_per_continent, Conclusie
 
 # Create a navigation menu in the sidebar
 page = st.sidebar.selectbox("Go to", ["Age Groups", "Interests per Continent", "Conclusie"])
